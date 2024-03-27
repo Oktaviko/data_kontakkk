45fe13242f4e5589b2ef4781e0566f87a05b5ce0
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-03-27 22:31:52 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:data_kontakkk/screen/hal_register.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Data Kontak",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Data Kontak"),
+          centerTitle: true,
+        ),
+        body: FormKontak()
+      ),
+    );
+  }
+}
