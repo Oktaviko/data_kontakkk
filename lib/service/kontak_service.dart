0bd70f487ef011cbdfffd0ba1dfc49e011f176ea
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-03-27 23:02:13 +0700

    create path controler container

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -1,9 +1,29 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
 class KontakService{
   final String baseUrl = 'https://paml.tatiumy.com/api/';
   final String endPoint = 'people';
 
   Uri getUri(String path){
     return Uri.parse("$baseUrl$path");
+  }
+
+  Future<http.Response> addPerson(Map<String, String> data, File? file) async {
+    var request = http.MultipartRequest(
+      'POST',
+      getUri(endPoint),
+    )
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'application/json';
+
+      if (file != null){
+        request.files.add(await http.MultipartFile.fromPath('gambar', file.Path));
+      }
+      else {
+        
+      }
   }
 }
 
