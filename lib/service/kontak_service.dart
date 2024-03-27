70fed9ce0f5c92f87378c6a55c614d85158449e5
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-03-27 22:48:49 +0700

    memanggil baseUrl dan pathnya

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -1,4 +1,10 @@
 class KontakService{
   final String baseUrl = 'https://paml.tatiumy.com/api/';
   final String endPoint = 'people';
+
+  Uri getUri(String path){
+    return Uri.parse("$baseUrl$path");
+  }
 }
+
+  
