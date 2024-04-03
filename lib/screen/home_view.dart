9b0cd446c14a52897c04aa0cd9bb3aec4dcb0e16
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-04-03 20:50:56 +0700

    add floating actionbutton

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:data_kontakkk/controller/kontak_controller.dart';
 import 'package:data_kontakkk/model/kontak.dart';
+import 'package:data_kontakkk/screen/hal_register.dart';
 
 import 'package:flutter/material.dart';
 
@@ -11,49 +12,55 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-
   final KontakController _controller = KontakController();
 
   @override
-   void initState() {
+  void initState() {
     super.initState();
     _controller.getPeople();
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Daftar Orang"),
-        ),
-        body: FutureBuilder<List<Kontak>>(
-          future: _controller.getPeople(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  Kontak person = snapshot.data![index];
-                  return ListTile(
-                    title: Text(person.nama),
-                    subtitle: Text(person.email),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(person.foto),
-                    ),
-                  );
-                },
-              );
-            }
-          },
-        ),
-        );
+      appBar: AppBar(
+        title: const Text("Daftar Orang"),
+      ),
+      body: FutureBuilder<List<Kontak>>(
+        future: _controller.getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Kontak person = snapshot.data![index];
+                return ListTile(
+                  title: Text(person.nama),
+                  subtitle: Text(person.email),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(person.foto),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const FormKontak()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
