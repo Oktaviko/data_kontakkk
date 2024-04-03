7a36d955b963b8de82d8d71a2d306adeb7ebfcdb
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-04-03 20:47:01 +0700

    add body

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:data_kontakkk/controller/kontak_controller.dart';
+import 'package:data_kontakkk/model/kontak.dart';
 
 import 'package:flutter/material.dart';
 
@@ -24,6 +25,34 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Daftar Orang"),
+        ),
+        body: FutureBuilder<List<Kontak>>(
+          future: _controller.getPeople(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  Kontak person = snapshot.data![index];
+                  return ListTile(
+                    title: Text(person.nama),
+                    subtitle: Text(person.email),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(person.foto),
+                    ),
+                  );
+                },
+              );
+            }
+          },
         ),
         );
   }
